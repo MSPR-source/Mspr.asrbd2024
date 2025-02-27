4b8138c498bdaed032e12e826113b84e9bc676f7
DigiCan-by-ELHOUZI <digican.elhouzi24@gmail.com>
2025-02-27 14:44:52 +0100

    version 10.5

--- Nester/app.py
+++ Nester/app.py
@@ -16,9 +16,9 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./users.db'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 
 db.init_app(app)
